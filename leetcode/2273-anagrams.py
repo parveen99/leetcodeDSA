fc7fd7acb0f07eb48d4361576c7a37fab7c043ff
athaparveen <athaparveen22@gmail.com>
2025-07-09 12:07:58 +0530

    prob 2273

---
+++ leetcode/2273-anagrams.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+#note: dont store len of words outside the condition, while the problems involves removing elements
+class Solution(object):
+    def removeAnagrams(self, words):
+        i = 1
+        while (i < len(words)):
+            if (Counter(words[i-1]) == Counter(words[i])):
+                words.pop(i)
+            else:
+                i += 1
+        return words
+
+
+s = Solution()
+words = ["yjonq","yqnoj","oyqjn","nqoyj","onjqy","joqyn","qynjo","jynoq"]
+result = s.removeAnagrams(words)
+print(result)
